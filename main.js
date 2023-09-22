225a71b086988291c33b13cf41be60464900f273
baddestboy <giwasodiq7@gmail.com>
2023-09-22 09:29:03 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let profileIcon = document.querySelector(".fa-globe");
+let olakunle = document.querySelector(".olakunle");
+
+profileIcon.addEventListener("click", () => {
+  olakunle.classList.toggle("active");
+});
